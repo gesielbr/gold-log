c6cfd17e1f30aa9d8eefecc3a8f198b44d79cb48
Gesiel Souza Oliveira <gesiel.br@gmail.com>
2021-05-11 19:22:18 -0300

    Js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function ($) {
+  //CAROUSEL
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    dots: true,
+    lazyLoad: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
+});
